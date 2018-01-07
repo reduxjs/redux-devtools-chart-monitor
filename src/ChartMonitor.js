746a68e88c61a34b062adfb902d4d315c1332b8d
Romain Séguy <romseguy@users.noreply.github.com>
2018-01-07 19:03:15 +0100

    Revert "Support React 15.5.0"

--- src/ChartMonitor.js
+++ src/ChartMonitor.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { PropTypes, Component } from 'react';
 import shouldPureComponentUpdate from 'react-pure-render/function';
 import * as themes from 'redux-devtools-themes';
 import { ActionCreators } from 'redux-devtools';
